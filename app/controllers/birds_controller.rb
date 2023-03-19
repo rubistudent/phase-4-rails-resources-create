5ac3deee8d410dbb281ca3b44f19e124156b3328
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2023-03-19 08:15:44 +0300

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,8 @@
 class BirdsController < ApplicationController
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
 
   # GET /birds
   def index
